c46e8a121827e7571a1a9c1a2b2b273877ac04f9
Mike <michaelakwobeng@gmail.com>
2022-09-19 16:28:28 +0000

    mongo d setup gitpod

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const config = require('config')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect('').then(()=>console.log("connected to Db"))
+.catch((err)=>console.log(err))
+
 
 app.get('/', (req, res)=>{
     res.send({data: 'data'})
